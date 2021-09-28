d3466fa7e864722aae6a46d9c2a3f7aacf0add35
Julien <julien.horatius@gmail.com>
2021-09-28 15:20:45 +0200

    add forward and backward button functions, change some CSS settings

--- assets/JS/media.js
+++ assets/JS/media.js
@@ -1,7 +1,10 @@
 var video = document.querySelector('.video');
 var juice = document.querySelector('.orange_bar');
 var btn = document.getElementById('play_pause');
+var fwdBtn = document.querySelector('.button2');
+var bwdBtn = document.querySelector('.button3');
 
+// Fonction Play/Pause 
 function togglePlayPause(){
     if(video.paused){
         btn.className = "pause";
@@ -12,9 +15,13 @@ function togglePlayPause(){
         video.pause();
     }
 }
+
+// EventListener Play/Pause
 btn.onclick = function(){
     togglePlayPause();
 }
+
+// EventListener Loading
 video.addEventListener('timeupdate', function(){
     var juicePos= video.currentTime / video.duration;
     juice.style.width = juicePos * 100 + "%";
@@ -22,10 +29,29 @@ video.addEventListener('timeupdate', function(){
         btn.className = 'play';
     }
 })
+
+// EventListener Backward/Forward
+bwdBtn.addEventListener('click', bwd, false);
+fwdBtn.addEventListener('click', fwd, false);
+
+// Fonction Backward/Forward
+function fwd(){
+    video.currentTime -= 3;
+}
+
+function bwd(){
+    video.currentTime += 3;
+    if(video.currentTime > video.duration){
+        fwdBtn.preventDefault(fwdBtn.onclick);
+    }
+}
+
+//EventListener Volume
 var currentVolume;
 currentVolume = document.getElementById("currentVolume");
 currentVolume.addEventListener("change", setvolume, false)
 
+// Fonction Volume 
 function setvolume(){
     video.volume = currentVolume.value/100;
 }
